f3732c4baa4b89e8298c9ae704110aec5cd0c941
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-08 22:07:19 -0500

    Fix UI elements of Ranking page

--- Ranking/RankingItem.js
+++ Ranking/RankingItem.js
@@ -6,7 +6,7 @@ const DeviceWidth = Dimensions.get('window').width;
 
 export const RankingItem = ({ max, cost, item, image, unit }) => {
 
-   const percentage = 1-(max-parseInt(cost))/max;
+   const percentage = 1-(max-parseInt(cost))/max < 0.01 ? 0.01 : 1-(max-parseInt(cost))/max;
 
     const numberWithCommas = (x) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -16,7 +16,7 @@ export const RankingItem = ({ max, cost, item, image, unit }) => {
         <View>
             <View style={{flexDirection: 'row'}}>
                 <Image
-                    style={{height: 45, width: 45, margin: 10}}
+                    style={{height: 45, width: 45, marginTop: 10, marginBottom: 10, marginLeft: 10, marginRight: 20}}
                     source={image}
                 />
                 <Progress.Bar
@@ -24,12 +24,12 @@ export const RankingItem = ({ max, cost, item, image, unit }) => {
                     width={DeviceWidth*0.80}
                     height={10}
                     borderColor="white"
-                    paddingTop={25}
+                    paddingTop={35}
                     color="#81CAFF"
                 />
             </View>
-            <View style={{flexDirection: 'row'}}>
-                <Text style={{fontSize: 13, marginLeft: 15, maxWidth: 200, color: '#000000'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={{fontSize: 13, marginLeft: 15, color: '#000000'}}>
                     {item}
                 </Text>
                 <Text
@@ -37,7 +37,7 @@ export const RankingItem = ({ max, cost, item, image, unit }) => {
                         fontSize: 14,
                         color: 'gray',
                         fontWeight: 'bold',
-                        marginLeft: 20
+                        marginRight: 20
                     }}
                 >
                     {numberWithCommas(cost)} {unit === 'L' ? 'Liters' : 'Gallons'}
