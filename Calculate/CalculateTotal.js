88f89d142c83a7c91667254d4a11d1c8947e4df3
MikeShuyang <wangshuy@usc.edu>
2021-11-22 20:44:08 -0800

    Merged from calculator branch

--- Calculate/CalculateTotal.js
+++ Calculate/CalculateTotal.js
@@ -2,59 +2,59 @@ import React from 'react';
 import {Text, Dimensions} from 'react-native';
 
 const numberWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 const DeviceWidth = Dimensions.get('window').width;
 
 export const CalculateTotal = ({value, unit, id, type}) => {
-    const stringUnit = (unit, id, type) => {
-        if (unit === 'L') {
-            //return type === 'individual' ? 'L. p/item' : 'L.';
-            if (
-                id === 'EDI' ||
-                id === 'EDF' ||
-                id === 'Drinks - All' ||
-                id === 'Drinks - Alc'
-            ) {
-                return type === 'individual' ? 'L. p/item' : 'L.';
-            } else {
-                //return 'L. p/kg';
-                return type === 'individual' ? 'L. p/item' : 'L.';
-            }
-        } else if (unit === 'G') {
-            //return type === 'individual' ? 'gal. p/item' : 'gal.';
-            if (
-                id === 'EDI' ||
-                id === 'EDF' ||
-                id === 'Drinks - All' ||
-                id === 'Drinks - Alc'
-            ) {
-                return type === 'individual' ? 'gal. p/item' : 'gal.';
-            } else {
-                //return 'gal. p/lb';
-                return type === 'individual' ? 'gal. p/item' : 'gal.';
-            }
-        }
-    };
+  const stringUnit = (unit, id, type) => {
+    if (unit === 'L') {
+      //return type === 'individual' ? 'L. p/item' : 'L.';
+      if (
+        id === 'EDI' ||
+        id === 'EDF' ||
+        id === 'Drinks - All' ||
+        id === 'Drinks - Alc'
+      ) {
+        return type === 'individual' ? 'L. p/item' : 'L.';
+      } else {
+        //return 'L. p/kg';
+          return type === 'individual' ? 'L. p/item' : 'L.';
+      }
+    } else if (unit === 'G') {
+      //return type === 'individual' ? 'gal. p/item' : 'gal.';
+      if (
+        id === 'EDI' ||
+        id === 'EDF' ||
+        id === 'Drinks - All' ||
+        id === 'Drinks - Alc'
+      ) {
+        return type === 'individual' ? 'gal. p/item' : 'gal.';
+      } else {
+        //return 'gal. p/lb';
+          return type === 'individual' ? 'gal. p/item' : 'gal.';
+      }
+    }
+  };
 
-    return (
-        <>
-            <Text
-                style={{
-                    borderColor: '#80CAFF',
-                    marginTop: 10,
-                    height: 50,
-                    borderWidth: 2,
-                    borderRadius: 20,
-                    width: DeviceWidth * 0.9,
-                    textAlign: 'center',
-                    fontSize: 20,
-                    paddingTop: 10,
-                    fontWeight: 'bold',
-                }}>
-                {numberWithCommas(value)} {stringUnit(unit, id, type)}
-            </Text>
-        </>
-    );
+  return (
+    <>
+      <Text
+        style={{
+          borderColor: '#80CAFF',
+          marginTop: 10,
+          height: 50,
+          borderWidth: 2,
+          borderRadius: 20,
+          width: DeviceWidth * 0.9,
+          textAlign: 'center',
+          fontSize: 20,
+          paddingTop: 10,
+          fontWeight: 'bold',
+        }}>
+        {numberWithCommas(value)} {stringUnit(unit, id, type)}
+      </Text>
+    </>
+  );
 };
