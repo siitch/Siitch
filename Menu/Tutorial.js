b8f63fdbda208041fb08204570c7f1690f44ed74
MikeShuyang <wangshuy@usc.edu>
2022-01-18 15:03:33 -0800

    Modified the last tutorial slide

--- Menu/Tutorial.js
+++ Menu/Tutorial.js
@@ -118,29 +118,36 @@ const Tutorial = ({navigation}) => {
         subtitle: '',
         backgroundColor: '#ffffff',
         image: (
-            <ScrollView style={{backgroundColor: 'white', marginTop: height - (height * 1.2)}}>
-              <ImageBackground style={{height:height
-                , width:width, flexDirection: 'column-reverse'}} source={require('../images2/Tutorial9.png')}>
-                <TouchableOpacity
-                    style={{
-                      width: 300,
-                      height: 50,
-                      color: '#ffffff',
-                      alignItems: "center",
-                      backgroundColor: "#70BF41",
-                      marginBottom: height/7,
-                      borderRadius: 25,
-                      alignSelf: 'center',
-                      justifyContent: 'center'
-                    }}
-                    onPress={() => {
-                      navigation.goBack("Landing Page")
-                    }}
-                >
-                  <Text style={{ color: "white", fontSize:25, fontFamily: 'Arial', fontWeight:'bold'}}>LET'S GO!</Text>
-                </TouchableOpacity>
-              </ImageBackground>
-            </ScrollView>
+          <View style={{backgroundColor: 'white', marginTop: height - (height * 1.2)}}>
+            <ImageBackground
+              style={{
+                width: width,
+                height: 3584 * width / 1656,
+                flexDirection: 'column-reverse'
+              }}
+              source={require('../images2/Tutorial9.png')}
+              resizeMode={'contain'}
+            >
+              <TouchableOpacity
+                style={{
+                  width: 300,
+                  height: 50,
+                  color: '#ffffff',
+                  alignItems: "center",
+                  backgroundColor: "#70BF41",
+                  borderRadius: 25,
+                  marginBottom: (1656 / 3584) * 3584 * (1 / 13),
+                  alignSelf: 'center',
+                  justifyContent: 'center'
+                }}
+                onPress={() => {
+                  navigation.goBack("Landing Page")
+                }}
+              >
+                <Text style={{ color: "white", fontSize:25, fontFamily: 'Arial', fontWeight:'bold'}}>LET'S GO!</Text>
+              </TouchableOpacity>
+            </ImageBackground>
+          </View>
         ),
       },
     ]}
