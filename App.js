4c11193ba4461f6ece16ce9086c1cc96d29d9cbc
MikeShuyang <wangshuy@usc.edu>
2022-03-07 16:35:32 -0800

    Naming updates, added fetching item list on app start and a global notification component.

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import 'react-native-gesture-handler';
-import React, {useEffect, Component} from 'react';
-import {useState} from 'react';
-import {View, Text, Image, Dimensions, Button, Pressable, ScrollView} from 'react-native';
+import React, {useEffect} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-import {landingdetails} from './LandingPage';
+import {LandingDetails} from './LandingPage';
 import {OnboardingScreen} from './OnboardingScreen';
 
 import { Provider as PaperProvider } from 'react-native-paper';
+import FlashMessage from "react-native-flash-message";
+import firebase from "firebase";
 
 const AppStack = createStackNavigator();
-
+global.globalList = []
 const App = () => {
-
 
   const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
 
@@ -29,29 +27,60 @@ const App = () => {
                 setIsFirstLaunch(false);
             }
         });
+        getData()
     }, []);
+
+  function getData () {
+    const config = {
+      apiKey: 'AIzaSyA0mAVUu-4GHPXCdBlqqVaky7ZloyfRARk',
+      authDomain: 'siitch-6b176.firebaseapp.com',
+      databaseURL: 'https://siitch-6b176.firebaseio.com',
+      projectId: 'siitch-6b176',
+      storageBucket: 'siitch-6b176.appspot.com',
+      messagingSenderId: '282599031511',
+      appId: '1:282599031511:web:bb4f5ca5c385550d8ee692',
+      measurementId: 'G-13MVLQ6ZPF',
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+    firebase
+      .database()
+      .ref('/')
+      .once('value', data => {
+        let fetchedData = data.val();
+        for (let item in fetchedData) {
+          if (item === 'Future Library') continue;
+          globalList.push({
+            name: item,
+          });
+        }
+      });
+  }
 
     if(isFirstLaunch === null){
         return null;
     }else if (isFirstLaunch === true) {
       return (
         <PaperProvider>
-        <NavigationContainer independent={true}>
-          <AppStack.Navigator headerMode="none">
-            <AppStack.Screen name = "Onboarding" component={OnboardingScreen} />
-            <AppStack.Screen name = "Landing Page" component={landingdetails} />
-          </AppStack.Navigator>
-        </NavigationContainer>
+          <NavigationContainer independent={true}>
+            <AppStack.Navigator headerMode="none">
+              <AppStack.Screen name = "Onboarding" component={OnboardingScreen} />
+              <AppStack.Screen name = "Landing Page" component={LandingDetails} />
+            </AppStack.Navigator>
+          </NavigationContainer>
+          <FlashMessage position="top" />
         </PaperProvider>
       );
     } else {
       return (
         <PaperProvider>
-        <NavigationContainer independent={true}>
-          <AppStack.Navigator headerMode="none">
-            <AppStack.Screen name = "Landing Page" component={landingdetails} />
-          </AppStack.Navigator>
-        </NavigationContainer>
+          <NavigationContainer independent={true}>
+            <AppStack.Navigator headerMode="none">
+              <AppStack.Screen name = "Landing Page" component={LandingDetails} />
+            </AppStack.Navigator>
+          </NavigationContainer>
+          <FlashMessage position="top" />
         </PaperProvider>
       );
     }
