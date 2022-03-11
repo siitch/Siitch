e595676eb4af10dd5d1512fb934d643ac1039489
MikeShuyang <wangshuy@usc.edu>
2022-03-11 00:36:30 -0800

    Dependency fix

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: ['react-native-reanimated/plugin']
+  };
 };
