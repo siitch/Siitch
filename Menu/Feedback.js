07fd6351ab6a6b9d4ae0788b00fa3b5eebe90f5d
MikeShuyang <wangshuy@usc.edu>
2023-05-07 15:23:23 -0700

    Updated the feedback link

--- Menu/Feedback.js
+++ Menu/Feedback.js
@@ -5,7 +5,7 @@ import { WebView } from 'react-native-webview';
 export const Feedback = ({ navigation }) => {
   return (
     <WebView
-      source={{ uri: 'https://docs.google.com/forms/d/e/1FAIpQLSfXTM0QORW9VGi7KE6bgNpNM8SGZgzsYu3YS1wZdi0oL6S-4A/viewform?vc=0&c=0&w=1&flr=0' }}
+      source={{ uri: 'https://docs.google.com/forms/d/e/1FAIpQLSeqChRf7XP46G5Dm6fvXrRUS7ucKI140yyTp_CBBxkElXss5g/viewform?usp=sf_link' }}
     />
   );
 }
