8b782b62173f4714b8c51aa24ef00a8b038582f0
MikeShuyang <wangshuy@usc.edu>
2024-05-29 12:22:52 +0800

    Fixed the archive [BABEL] error by removing the react-native-paper babel plugin. Updated the ios project settings according to the template ( https://github.com/expo/expo/tree/main/templates/expo-template-bare-minimum/ios )

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,5 @@ module.exports = function(api) {
   return {
     presets: ['module:@react-native/babel-preset'],
     plugins: ['react-native-reanimated/plugin'],
-    env: {
-      production: {
-        presets: ['react-native-paper/babel'],
-      }
-    },
   };
 };
