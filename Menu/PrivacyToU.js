559d760e84f809e8e822a062fc7061e88156a7d1
MikeShuyang <wangshuy@usc.edu>
2024-10-20 10:24:45 +0800

    Privacy and ToU updates
    - Updated the content of the privacy and ToU list
    - Added index to the list to fix the unique identifier error

--- Menu/PrivacyToU.js
+++ Menu/PrivacyToU.js
@@ -8,12 +8,11 @@ export const PrivacyToU = () => {
     "The Siitch app is currently in BETA and accessible by invite-only at this time.",
     "There is no account to create to use the app.",
     "No names, emails or passwords required.",
-    "Given we don’t have defined ‘users’ in the app, the app does not collect or track any personal information.",
+    "Given we don’t have defined ‘users’ in the app, THE APP DOES NOT COLLECT OR TRACK ANY PERSONAL INFORMATION.",
     "No information is stored in the app.",
     "The app allows anyone to take a picture of, for example, an apple, to see its environmental cost, but we cannot see the photos you take, nor are the pictures stored in the app, or on your phone.",
     "We use Google Analytics to track and gather aggregate usage data - for example - what features are used the most, and equally, the least, so we can improve our services.",
     "Google Analytics will show us what state or country users are in, but that information is not linkable to any identified individual.",
-    "No information will be used for marketing purposes.",
     "Each build is available to test for up to 90 days, starting from the day we upload each build. You can see how many days you have left for testing under the app name in TestFlight. TestFlight will notify you each time a new build is available and will include instructions on the recent updates.",
     "Your use of this app means you agree to the above and the full Privacy policy and Terms of use. If you do not agree with the above or anything in the full policies linked below, please do not use the app."
   ];
@@ -33,9 +32,9 @@ export const PrivacyToU = () => {
       <View style={{
         marginHorizontal: '8%',
       }}>
-        {privacyToUList.map((item) => {
+        {privacyToUList.map((item, index) => {
           return(
-            <View style={{flexDirection: 'row'}}>
+            <View key={index} style={{flexDirection: 'row'}}>
               <View style={{marginRight: 5}}>
                 <Text style={{fontSize: 7, marginTop: 6.5}}>●</Text>
               </View>
